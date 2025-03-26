511b275e97e3394af769985f2ea105b3912b9d8d
Noah S <n_solomon@mail.fhsu.edu>
2025-03-26 11:04:56 -0700

    updated codes

--- config/Database.php
+++ config/Database.php
@@ -1,35 +1,37 @@
-<?php
-class Database {
+<?php 
+  class Database {
     private $conn;
     private $host;
-    private $dbname;
+    private $db_name;
     private $username;
     private $password;
-    
 
     public function __construct() {
-        $this->username = getenv('USERNAME');
-        $this->password = getenv('PASSWORD');
-        $this->dbname = getenv('DBNAME');
-        $this->host = getenv('HOST');
-        
-    }
-  
-      public function connect() {
-        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
-        if (is_null($this->conn)) {
-          try { 
-            $this->conn = new PDO($dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          } catch(PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
-          }
-        } 
-        return $this->conn;    
-      }
+      $this->username = getenv('USERNAME');
+      $this->password = getenv('PASSWORD');
+      $this->db_name = getenv('DBNAME');
+      $this->host = getenv('HOST');
+      
     }
     
-}
+    // DB Connect
+    public function connect() {
+      if ($this->conn) {
+        return $this->conn;
+      } else {
+        // $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+        $dsn = "pgsql:host={$this->host};dbname={$this->db_name}";
+
+        try {
+          $this->conn = new PDO($dsn, $this->username, $this->password);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+          echo 'Connection Error: ' . $e->getMessage();
+        }
+        return $this->conn;
+      }
+    }
+  }
 
 // $this->port = getenv('PORT');
 //  private $port;
