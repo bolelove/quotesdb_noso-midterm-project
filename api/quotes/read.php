a60418caf4d71903e1a6705fc96d6fb6fac0bd3e
Noah S <n_solomon@mail.fhsu.edu>
2025-03-26 20:26:51 -0700

    8:26 code update

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -6,41 +6,41 @@
   $database = new Database();
   $db = $database->connect();
 
-  // Instantiate quote   pbject
+  // Instantiate quote object
   $quote  = new Quote($db);
 
   // Quote read query
-  $result = $quote ->read();  
+  $result = $quote->read();  
 
   // Get row count
   $num = $result->rowCount();
 
   // Check if there are any quotes
   if($num > 0) {
-    // Initialize quote array and the 'data' key as an array
-    $quotes_arr = array();
-    $quotes_arr['data'] = array();  // Initialize 'data' as an empty array
+        // Initialize quote array and the 'data' key as an array
+        $quotes_arr = array();
+        $quotes_arr['data'] = array();  // Initialize 'data' as an empty array
 
-    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-      extract($row);
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          extract($row);
 
-      $quote_item = array(
-        'id' => $id,
-        'quote' => $quote, 
-        'author' => $author,
-        'category' => $category
-      );
+          $quote_item = array(
+            'id' => $id,
+            'quote' => $quote, 
+            'author' => $author,
+            'category' => $category
+          );
 
-      // Push to "data" key of $quotes_arr
-      array_push($quotes_arr['data'], $quote_item);
-    }
+          // Push to "data" key of $quotes_arr
+          array_push($quotes_arr['data'], $quote_item);
+        }
 
-    // Turn to JSON & output
-    echo json_encode($quotes_arr);
-} else {
-    // No quotes found
-    echo json_encode(
-      array('message' => 'No quotes Found')
-    );
-}
+        // Turn to JSON & output
+        echo json_encode($quotes_arr);
+  } else {
+        // No quotes found
+        echo json_encode(
+          array('message' => 'No quotes Found')
+        );
+  }
 ?>
